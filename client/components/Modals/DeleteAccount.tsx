b3f365b7c5c5e397ba0c526e06943f6fab389b02
Raheem Kawojue <kawojue08@gmail.com>
2023-09-28 13:59:01 +0100

    [add] delete account modal

---
+++ client/components/Modals/DeleteAccount.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+import Modal from './Modal'
+
+const DeleteAccount: FC<ModalComponent> = ({ get, set, title, data }) => {
+    return (
+        <Modal
+            get={get}
+            set={set}>
+
+        </Modal>
+    )
+}
+
+export default DeleteAccount
