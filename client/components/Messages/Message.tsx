7b64e8ca8488cedb4a8f013afc26a98e3849183c
Raheem Kawojue <kawojue08@gmail.com>
2023-09-28 22:49:25 +0100

    [fix] styles

--- client/components/Messages/Message.tsx
+++ client/components/Messages/Message.tsx
@@ -9,29 +9,55 @@ import { BsDownload } from '@/public/icons/ico'
 const Message: FC<{ message: MessageStates }> = ({ message }) => {
     const captureDivRef = useRef<HTMLDivElement>(null)
 
-    const downloadDiv = (id: string) => {
-        const divToCapture = captureDivRef.current
+    function downloadHTMLTemplate(id: string, messageTexts: string) {
+        //         const containerStyles = `
+        //     display: flex;
+        //     justify-content: center;
+        //     align-items: center;
+        //     width: 100%;
+        //     min-height: 220px;
+        //     border-radius: 30px;
+        //     padding: 3px;
+        //     background-color: #yourBackgroundColor;
+        //     box-shadow: 10px 10px 18px 0 rgba(0, 0, 0, 0.3),
+        //                 inset -10px -10px 18px 0 rgba(0, 0, 0, 0.3),
+        //                 inset 10px 10px 18px 0 rgba(255, 255, 255, 0.2);
+        //   `
 
-        if (!divToCapture) {
-            return
-        }
+        //         const htmlContent = `
+        //     <div style="${containerStyles}">
+        //       <div class="your-prompt-class">${messageTexts}</div>
+        //       <span style="position: absolute; top: 2px; left: 3px; font-size: 12px; color: #yourTextColor;">
+        //         ${getPeriod(message.date)} ago
+        //       </span>
+        //     </div>
+        //   `
 
-        html2Canvas(divToCapture).then((canvas) => {
-            const dataURL = canvas.toDataURL('image/png')
+        //         const blob = new Blob([htmlContent], { type: 'text/html' })
 
-            const downloadLink = document.createElement('a')
-            downloadLink.href = dataURL
-            downloadLink.download = `memome_${id}.png`
-            downloadLink.click()
-        })
+        //         const canvas = document.createElement('canvas')
+        //         const ctx = canvas.getContext('2d')
+        //         const img = new Image()
+        //         img.src = URL.createObjectURL(blob)
 
-        // trust me - I am coming back to it.
+        //         img.onload = () => {
+        //             canvas.width = img.width
+        //             canvas.height = img.height
+        //             ctx.drawImage(img, 0, 0)
+        //             canvas.toBlob((pngBlob) => {
+        //                 const downloadLink = document.createElement('a')
+        //                 downloadLink.href = URL.createObjectURL(pngBlob)
+        //                 downloadLink.download = `memome_${id}.png`
+        //                 downloadLink.click()
+        //             }, 'image/png')
+        //         }
     }
+
 
     return (
         <>
             <article
-                className={` ${message.files.length === 0 && 'flex items-center justify-center'} relative sm:w-[280px] md:w-[350px] sm:min-h-[235px] md:min-h-[250px] min-h-[220px] rounded-[30px] p-3 bg-clr-11 w-full`}
+                className={` ${message.files.length === 0 && 'flex items-center justify-center'} relative sm:min-h-[235px] md:min-h-[250px] min-h-[220px] rounded-[30px] p-3 bg-clr-11 max-w-[300px] w-[90vw]`}
                 style={{
                     boxShadow: `10px 10px 18px 0 rgba(0, 0, 0, 0.3), inset -10px -10px 18px 0 rgba(0, 0, 0, 0.3), inset 10px 10px 18px 0 rgba(255, 255, 255, 0.2)`
                 }}>
@@ -47,13 +73,12 @@ const Message: FC<{ message: MessageStates }> = ({ message }) => {
                                 }}
                                 dangerouslySetInnerHTML={{ __html: message.texts }}
                             />}
-                        <button
-                            onClick={() => downloadDiv(message.id)}
+                        {/* <button
+                            onClick={() => downloadHTMLTemplate(message.id, message.texts as string)}
                             className='absolute bottom-3 right-4 text-lg font-semibold text-black'>
                             <BsDownload />
-                        </button>
-                    </>
-                    :
+                        </button> */}
+                    </> :
                     <article className='h-full flex flex-col justify-between'>
                         {message.texts &&
                             <div
