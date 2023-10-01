219a3f0ca1925dd473d7a11ed16d67f204af9063
Raheem Muyiwa <kawojue08@gmail.com>
2023-10-01 19:38:26 +0100

    Update logout.ts

--- client/lib/logout.ts
+++ client/lib/logout.ts
@@ -2,7 +2,7 @@ import axios from '@/app/api/axios'
 import { AxiosResponse } from 'axios'
 
 const logout = async () => {
-    return await axios.get('/auth/logout')
+    return await axios.post('/auth/logout')
         .then((res: AxiosResponse) => res)
 }
 
