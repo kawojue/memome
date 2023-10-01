ed84eda116ab42c7b0598cec4919c4e28a7c9aa0
Raheem Kawojue <kawojue08@gmail.com>
2023-10-01 01:47:50 +0100

    [edit] levels

--- generative/db/levels/message.level.ts
+++ generative/db/levels/message.level.ts
@@ -1,29 +1,41 @@
 export const messageLevels = [
-    { id: 1, name: 'Novice Enthusiast', pointRange: [0, 32] },
-    { id: 2, name: 'Luminary Artist', pointRange: [32, 64] },
-    { id: 3, name: 'Celestial Luminary', pointRange: [64, 96] },
-    { id: 4, name: 'Masterful Messenger', pointRange: [96, 128] },
-    { id: 5, name: 'Mystical Messenger', pointRange: [128, 160] },
-    { id: 6, name: 'Eloquent Emissary', pointRange: [160, 192] },
-    { id: 7, name: 'Cheerful Chatterbox', pointRange: [192, 224] },
-    { id: 8, name: 'Charismatic Chimer', pointRange: [224, 256] },
-    { id: 9, name: 'Charming Communicator', pointRange: [256, 288] },
-    { id: 10, name: 'Captivating Communicator', pointRange: [288, 320] },
-    { id: 11, name: 'Starry Performer', pointRange: [320, 352] },
-    { id: 12, name: 'Enchanted Voyager', pointRange: [352, 384] },
-    { id: 13, name: 'Virtuoso Voyager', pointRange: [384, 416] },
-    { id: 14, name: 'Enchanted Virtuoso', pointRange: [416, 448] },
-    { id: 15, name: 'Anonymity Virtuoso', pointRange: [448, 480] },
-    { id: 16, name: 'Anonymous Aristocrat', pointRange: [480, 512] },
-    { id: 17, name: 'Quirky Quester', pointRange: [512, 544] },
-    { id: 18, name: 'Wordsmith Wanderer', pointRange: [544, 576] },
-    { id: 19, name: 'Apprentice Entertainer', pointRange: [576, 608] },
-    { id: 20, name: 'Jovial Jester', pointRange: [608, 640] },
-    { id: 21, name: 'Anonymous Aficionado', pointRange: [640, 672] },
-    { id: 22, name: 'Merry Message Maven', pointRange: [672, 704] },
-    { id: 23, name: 'Whimsy Wizard', pointRange: [704, 736] },
-    { id: 24, name: 'Whimsical Wordsmith', pointRange: [736, 768] },
-    { id: 25, name: 'Grand Illuminator', pointRange: [768, 800] }
+    { id: 1, name: 'Novice Enthusiast', pointRange: [0, 11] },
+    { id: 2, name: 'Luminary Artist', pointRange: [11, 22] },
+    { id: 3, name: 'Celestial Luminary', pointRange: [22, 33] },
+    { id: 4, name: 'Masterful Messenger', pointRange: [33, 44] },
+    { id: 5, name: 'Mystical Messenger', pointRange: [44, 55] },
+    { id: 6, name: 'Eloquent Emissary', pointRange: [55, 66] },
+    { id: 7, name: 'Cheerful Chatterbox', pointRange: [66, 77] },
+    { id: 8, name: 'Charismatic Chimer', pointRange: [77, 88] },
+    { id: 9, name: 'Charming Communicator', pointRange: [88, 99] },
+    { id: 10, name: 'Captivating Communicator', pointRange: [99, 110] },
+    { id: 11, name: 'Starry Performer', pointRange: [110, 121] },
+    { id: 12, name: 'Enchanted Voyager', pointRange: [121, 132] },
+    { id: 13, name: 'Virtuoso Voyager', pointRange: [132, 143] },
+    { id: 14, name: 'Enchanted Virtuoso', pointRange: [143, 154] },
+    { id: 15, name: 'Anonymity Virtuoso', pointRange: [154, 165] },
+    { id: 16, name: 'Anonymous Aristocrat', pointRange: [165, 176] },
+    { id: 17, name: 'Quirky Quester', pointRange: [176, 187] },
+    { id: 18, name: 'Wordsmith Wanderer', pointRange: [187, 198] },
+    { id: 19, name: 'Apprentice Entertainer', pointRange: [198, 209] },
+    { id: 20, name: 'Jovial Jester', pointRange: [209, 220] },
+    { id: 21, name: 'Anonymous Aficionado', pointRange: [220, 231] },
+    { id: 22, name: 'Merry Message Maven', pointRange: [231, 242] },
+    { id: 23, name: 'Whimsy Wizard', pointRange: [242, 253] },
+    { id: 24, name: 'Whimsical Wordsmith', pointRange: [253, 264] },
+    { id: 25, name: 'Grand Illuminator', pointRange: [264, 275] }
 ]
 
 // Scraped Online
+
+// const newLevels = messageLevels.map((levels, index) => {
+//     const offset = 11 as const
+//     const lowerBound = index * offset
+//     const upperBound = (index + 1) * offset
+
+//     return {
+//         ...levels,
+//         id: index + 1,
+//         pointRange: [lowerBound, upperBound]
+//     }
+// })
