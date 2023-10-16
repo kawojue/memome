e70bd80d8e0afd450f03298f590ac2d32d432da4
Raheem Kawojue <kawojue08@gmail.com>
2023-10-16 01:38:59 +0100

    [edit] client page

--- client/components/Modals/Modal.tsx
+++ client/components/Modals/Modal.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { FC, Fragment } from 'react'
 import { poppins } from '@/public/fonts/f'
 import { ImCancelCircle } from '@/public/icons/ico'
@@ -6,30 +7,28 @@ import { Dialog, Transition } from '@headlessui/react'
 const Modal: FC<ModalProps> = ({ children, get, set }) => {
     return (
         <Transition appear show={get} as={Fragment}>
-            <Dialog as="div" className="relative z-[999]" onClose={() => set(false)}>
-                <Transition.Child
-                    as={Fragment}
+            <Dialog as="div"
+                className="relative z-[99999]"
+                onClose={() => set(false)}>
+                <Transition.Child as={Fragment}
                     enter="ease-out duration-300"
                     enterFrom="opacity-0"
                     enterTo="opacity-100"
                     leave="ease-in duration-200"
                     leaveFrom="opacity-100"
-                    leaveTo="opacity-0"
-                >
+                    leaveTo="opacity-0">
                     <div className="fixed inset-0 bg-black bg-opacity-25" />
                 </Transition.Child>
 
                 <div className="fixed inset-0 overflow-y-auto">
                     <div className="flex min-h-full items-center justify-center p-4 text-center">
-                        <Transition.Child
-                            as={Fragment}
+                        <Transition.Child as={Fragment}
                             enter="ease-out duration-300"
                             enterFrom="opacity-0 scale-95"
                             enterTo="opacity-100 scale-100"
                             leave="ease-in duration-200"
                             leaveFrom="opacity-100 scale-100"
-                            leaveTo="opacity-0 scale-95"
-                        >
+                            leaveTo="opacity-0 scale-95">
                             <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-clr-12 p-6 text-left align-middle shadow-xl transition-all">
                                 <button
                                     className={`${poppins.className} absolute top-2 right-2 p-2 bg-clr-4 text-clr-0 font-semibold text-lg md:text-xl trans hover:text-clr-6 hover:bg-clr-5 rounded-full`}
