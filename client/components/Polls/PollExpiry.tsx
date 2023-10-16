7ce932a8bad38cd96af070445780168fe40307d3
Raheem Kawojue <kawojue08@gmail.com>
2023-10-16 01:42:14 +0100

    refactored

--- client/components/Polls/PollExpiry.tsx
+++ client/components/Polls/PollExpiry.tsx
@@ -3,17 +3,18 @@ import notify from '@/utils/notify'
 import axios from '@/app/api/axios'
 import Modal from '../Modals/Modal'
 import { LoaderThree } from '../Loader'
+import { usePoll } from '@/utils/store'
 import throwError from '@/utils/throwError'
 import { FC, useEffect, useState } from 'react'
 import { AxiosError, AxiosResponse } from 'axios'
 import { poppins, questrial } from '@/public/fonts/f'
-import { useModalStore, usePoll } from '@/utils/store'
 
-const PollExpiry: FC<{ pollId: string }> = ({ pollId }) => {
+const PollExpiry: FC<PollExpiryProps> = ({
+    pollId, pollExpiryModal, setPollExpiryModal
+}) => {
     const { expiry, setExpiry } = usePoll()
     const [load, setLoad] = useState<boolean>(false)
     const [defaultExpiry, setDefaultExpiry] = useState<string>()
-    const { pollExpiryModal, setPollExpiryModal } = useModalStore()
 
     useEffect(() => {
         const currentDate = new Date()
@@ -55,8 +56,8 @@ const PollExpiry: FC<{ pollId: string }> = ({ pollId }) => {
             </h3>
             <article className='w-full my-3 flex justify-center items-center'>
                 <input
-                    value={expiry || defaultExpiry}
                     type='datetime-local'
+                    value={expiry || defaultExpiry}
                     onChange={(e) => setExpiry(e.target.value)}
                     className={`${questrial.className} tracking-wider font-medium outline-none px-1 py-0.5 rounded-md text-clr-16 bg-clr-0 md:text-base text-sm`}
                 />
