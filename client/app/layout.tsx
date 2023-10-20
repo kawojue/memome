c1d8389774b83301a11a5fb4357a2a37c01ca9d0
Raheem Kawojue <kawojue08@gmail.com>
2023-10-20 23:10:41 +0100

    [init] google analytics

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,14 +1,16 @@
 import './globals.css'
+import ReactGA from 'react-ga4'
 import type { Metadata } from 'next'
 import IsAuth from '@/components/IsAuth'
 import { Toaster } from 'react-hot-toast'
-import GAnalytics from '@/components/GAnalytics'
 import QueryProvider from '@/components/QueryProvider'
 
 export const metadata: Metadata = {
   title: 'MemoMe',
   description: 'An Ultimate Anonymous Platform for Secure Communication, Polls, and Content Control.',
 }
+
+ReactGA.initialize(process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID as string)
 
 export default function RootLayout({
   children,
@@ -17,9 +19,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <GAnalytics />
-      </head>
       <body className="min-h-screen">
         <Toaster
           position="top-center"
