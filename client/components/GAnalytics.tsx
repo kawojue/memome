b197609c52ac87be850071d14ac61f4693afb5e0
Raheem Muyiwa <kawojue08@gmail.com>
2023-10-20 15:35:56 +0100

    [add] ids

--- client/components/GAnalytics.tsx
+++ client/components/GAnalytics.tsx
@@ -5,9 +5,10 @@ const GAnalytics = () => {
     return (
         <>
             <Script async
+                id='ga'
                 src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID!}`}
             />
-            <Script>
+            <Script id='google-analytics'>
                 {`
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
